b3f8d6e703e9a0b293051f8c28f8398a17a986c1
Sky-Macbook <parkparkhaneul@gmail.com>
2021-03-28 17:17:51 +0900

    working on StudentSelectionPanel! adding 2 students done!

--- src/main/ui/StudentSelectionPanel.java
+++ src/main/ui/StudentSelectionPanel.java
@@ -11,6 +11,7 @@ public class StudentSelectionPanel extends JPanel {
     JPanel panel;
     JButton choose0;
     Image student1;
+    Image student2;
     JButton choose2;
     JButton choose3;
     JButton choose4;
@@ -34,9 +35,11 @@ public class StudentSelectionPanel extends JPanel {
             }
         });
 
-        choose2 = new JButton("When done, press 'a' to progress");
+        choose2 = new JButton("When done, press 'a' to progress on the module");
         choose2.setBounds(30, 60, 20, 20);
         choose2.setBackground(Color.blue);
+
+        //student 1
 
         student1 = new ImageIcon(this.getClass()
                 .getResource("/data/student1.png"))
@@ -48,15 +51,37 @@ public class StudentSelectionPanel extends JPanel {
         stu1Label.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                System.out.println("halajl");
+                System.out.println("1pressed");
                 letMeGraduate.putNewStudent1();
                 System.out.println(letMeGraduate.getStuList().getStuList().size());
                 progressOkay = true;
             }
         });
+
+        //student2
+
+        student2 = new ImageIcon(this.getClass()
+                .getResource("/data/student2.png"))
+                .getImage();
+        student2 = student2.getScaledInstance(50,50, Image.SCALE_SMOOTH);
+        ImageIcon stu2imageIcon = new ImageIcon(student2);
+
+        JLabel stu2Label = new JLabel(stu2imageIcon);
+        stu2Label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("2pressed");
+                letMeGraduate.putNewStudent1();
+                letMeGraduate.putNewStudent2();
+                System.out.println(letMeGraduate.getStuList().getStuList().size());
+                progressOkay = true;
+            }
+        });
+
         panel.add(choose0);
         panel.add(choose2);
         panel.add(stu1Label);
+        panel.add(stu2Label);
         frame.add(panel);
         frame.setSize(400, 400);
         frame.setLayout(null);
